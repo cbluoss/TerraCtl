2718569e625d8ddfc557a7c0075194d07df1f38b
yourcoke <deinkoks@gmail.com>
2020-10-23 21:37:07 +0200

    initial push

---
+++ Ctl.py
@@ -0,0 +1,61 @@
+from pcf8575 import PCF8575
+from rpi_ws281x import *
+import time
+
+# Main Program managing the terrarium systems.
+# Designed to run on:
+# * 1 raspberry pi (Zero)
+# * 1 PCF8575 16Port IO-Expander via i2c controlling 16 Relais (4x 230V power sockets, a 3.3v psu for some high power leds, 3 sets of LEDs + 2 general purpose 5V lines, 2 currently not in use)
+# * a 46 WS2812b Strip
+# * 2 DHT22 (not yet connected)
+# * a SSD1306 128x64 oled display (not yet connected)
+# Default state: everything off except the pi.
+#
+# I2C configuration (currently just the IO expander)
+I2C_PORT = 1
+PCF_ADDR = 0x20
+# actually  wrong side would be turned on (the 230c relais)
+# True = off (True|High is default at start)
+# pcf.port =  [True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False]
+
+# LED strip configuration:
+LED_COUNT      = 46      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+
+def boot_sequence(strip, duration= 10):
+    rainbow(strip)
+    time.sleep((duration))
+    colorWipe(strip, Color(0,0,0), 10)
+
+if __name__ == "__main__":
+    # Initialize the IO-Expander
+    pcf = PCF8575(I2C_PORT, PCF_ADDR)
+
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    boot_sequence(strip)
