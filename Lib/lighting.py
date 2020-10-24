d636b59fe404a38379c6007ff725ff7ca9007bb2
yourcoke <deinkoks@gmail.com>
2020-10-24 14:59:04 +0200

    added boot effect.

--- Lib/lighting.py
+++ Lib/lighting.py
@@ -156,4 +156,8 @@ class Lighting:
         for i in range(self.led_count):
             self.strip[i] = (255,0,0)
             time.sleep(duration/self.led_count)
-    
+        self.strip.fill((0,0,0))
+        time.sleep(0.01)
+        self.strip.fill((255,0,0))
+        time.sleep(0.01)
+        self.strip.fill((0,0,0))
