6062405ce6c38ec72b90ee1092319012b2397b1e
yourcoke <deinkoks@gmail.com>
2020-10-24 18:51:12 +0200

    daily schedule added

--- Ctl.py
+++ Ctl.py
@@ -17,29 +17,64 @@ TRIGGER = IntervalTrigger(hours=24)
 
 def event_sunrise(hw):
     logging.info("start sunrise")
-    hw.pcf.set_socket("BLUE", True) #Pump
-    hw.pcf.set_socket("GREEN", True) #Fog
+    try:
+        hw.pcf.set_socket("BLUE", True) #Pump
+        hw.pcf.set_socket("GREEN", True) #Fog
+    except IOError:
+        logging.debug("IO-Error")
+        sleep(5)
+        try:
+            hw.pcf.set_socket("BLUE", True) #Pump
+            hw.pcf.set_socket("GREEN", True) #Fog
+        except IOError:
+            pass
     hw.effect_color_fade(color_from=(5,5,5), color_to=(255,60,10),  delay_ms=150, steps=512)
 
 
 def event_sunset(hw):
     logging.info("start sunset")
     hw.effect_color_fade(color_from=(255,60,10), color_to=(5,5,5),  delay_ms=150, steps=512)
-    hw.pcf.set_socket("BLUE", False)
-    hw.pcf.reset() #shut everything off
+    try:
+        hw.pcf.set_socket("BLUE", False)
+        hw.pcf.reset() #shut everything off
+    except IOError:
+        logging.debug("IO-Error")
+        sleep(5)
+        try:
+            hw.pcf.set_socket("BLUE", False)
+            hw.pcf.reset() #shut everything off
+        except IOError:
+            pass
     while datetime.now.day == NOW.day:
         hw.effect_twinkle(color=(10,20,30), count=2, delay_ms=200 ,duration=60*1000, bg_color=(5,5,5))
 
 def event_disable_fog(hw):
     logging.info("stop fog")
-    hw.pcf.set_socket("GREEN", False)
-    hw.pcf.set_full_spec(True)
-
+    try:
+        hw.pcf.set_socket("GREEN", False)
+        hw.pcf.set_full_spec(True)
+    except IOError:
+        logging.debug("IO-Error")
+        sleep(5)
+        try:
+            hw.pcf.set_socket("GREEN", False)
+            hw.pcf.set_full_spec(True)
+        except IOError:
+            pass
 def event_high_noon(hw):
     logging.info("High Noon (1 Hour)")
-    hw.pcf.set_white(True)
-    sleep(60*60)
-    hw.pcf.set_white(False)
+    try:
+        hw.pcf.set_white(True)
+        sleep(60*60)
+        hw.pcf.set_white(False)
+    except IOError:
+        logging.debug("IO-Error")
+        sleep(5)
+        try:
+            hw.pcf.set_socket("GREEN", False)
+            hw.pcf.set_full_spec(True)
+        except IOError:
+            pass
 
 if __name__ == "__main__":
     logging.info("start main")
@@ -50,8 +85,8 @@ if __name__ == "__main__":
 
     scheduler.add_job(func=event_sunrise,trigger=TRIGGER, next_run_time=SUNRISE_AT, name="sunrise", args=[HW,])
     scheduler.add_job(func=event_sunset, trigger=TRIGGER, next_run_time=SUNSET_AT, name="sunset", args=[HW,])
-    scheduler.add_job(func=event_disable_fog(), trigger=TRIGGER, next_run_time=FOG_STOP_AT, name="fog_stop", args=[HW,])
-    scheduler.add_job(func=event_high_noon(), trigger=TRIGGER, next_run_time=HIGHNOON_AT, name="highnoon", args=[HW,])
+    scheduler.add_job(func=event_disable_fog, trigger=TRIGGER, next_run_time=FOG_STOP_AT, name="fog_stop", args=[HW,])
+    scheduler.add_job(func=event_high_noon, trigger=TRIGGER, next_run_time=HIGHNOON_AT, name="highnoon", args=[HW,])
 
     logging.info(scheduler.print_jobs())
 
