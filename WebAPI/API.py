7041c2f27d528eda47459054c7506fac64fedf6e
yourcoke <deinkoks@gmail.com>
2020-11-01 20:05:17 +0100

    add first route

--- WebAPI/API.py
+++ WebAPI/API.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from datetime import datetime
 from db import SQLALCHEMY_DATABASE_URI
 import json
