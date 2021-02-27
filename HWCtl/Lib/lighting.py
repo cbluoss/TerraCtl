27ec4770d44892bfe712ba31adcde292d1d53d25
yourcoke <deinkoks@gmail.com>
2021-02-27 14:53:57 +0100

    String formatting

--- HWCtl/Lib/lighting.py
+++ HWCtl/Lib/lighting.py
@@ -61,7 +61,7 @@ class Display_Wrapper:
 
         i = 16
         for s in sensor_data:
-            self.drawing.text((x, top + i), "T: %s°C | H: %s%%" % (s['temperature'], s['humidity']), font=self.font, fill=255)
+            self.drawing.text((x, top + i), "T: %2.1f°C | H: %i%%" % (s['temperature'], s['humidity']), font=self.font, fill=255)
             i += 8
 
         self.display.image(self.image)
